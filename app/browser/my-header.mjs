ee274de23152cd4ccd559b873bdc5fbcaf3786ed
macdonst <simon.macdonald@gmail.com>
2023-09-29 18:20:47 -0400

    ack

---
+++ app/browser/my-header.mjs
@@ -0,0 +1,45 @@
+// import CustomElement from '@enhance/custom-element'
+import CustomElement from '@enhance-labs/custom-element'
+
+class MyHeader extends CustomElement {
+  constructor() {
+    super()
+    this.heading = ''
+    this.header = this.querySelector('h1')
+  }
+
+  render({ html, state  }) {
+    console.log('client render got called')
+    const { attrs={} } = state
+    const { heading='default' } = attrs
+    return html`
+      <style>
+        :host {
+          color: red;
+        }
+        p, div {
+          color: purple;
+        }
+        @media screen and (min-width: 48em) {
+          p {
+            color: orange;
+          }
+        }
+      </style>
+      <h1>${heading}</h1>
+      <p>Inner Text</p>
+    `
+  }
+
+  static get observedAttributes() {
+    return [ 'heading' ]
+  }
+
+  headingChanged(value) {
+    this.header.textContent = value
+  }
+}
+
+export const render = MyHeader.prototype.render;
+
+customElements.define('my-header', MyHeader)
---
+++ app/browser/sample-component.mjs
@@ -0,0 +1,3 @@
+import SampleComponent from '@enhance-labs/sample-component'
+export default SampleComponent
+export const render = SampleComponent.prototype.render;
